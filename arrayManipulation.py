d5b67bbddf53df51314788a1a62b22c624e7895c
VictorSully79 <victorsullivan@hotmail.com>
2021-09-10 15:00:50 -0700

    challenge 9

---
+++ arrayManipulation.py
@@ -0,0 +1,20 @@
+# arrayManipulation
+# Victor Sullivan
+# 20210909
+# Manipulate an array in python
+
+randomStrings = ["this", "is", "so", "much", "fun", "for reelz", "yo", "I", "mean", "it"]
+
+print("This is position 4: ", randomStrings[3])
+
+print("This is positions 6 - 10: ", randomStrings[5:10])
+
+print("Here is the old array: ", randomStrings)
+
+randomStrings[6] = "onion"
+
+print("Here is the new array: ", randomStrings)
+
+
+
+
