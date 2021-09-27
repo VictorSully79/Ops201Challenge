2fd3b05e5c7775452693f1f46d070914d1917483
VictorSully79 <victorsullivan@hotmail.com>
2021-09-27 00:00:25 -0700

    fixed typos

--- directoryCreation.py
+++ directoryCreation.py
@@ -3,7 +3,7 @@
 # 20210908
 # Create a Python script that generates all directories, sub-directories and files for a user-provided directory path
 
-# Import libraries
+# Import the Library
 import os
 
 # Variables
@@ -13,18 +13,15 @@ path=input("FilePath goes here: ")
 def dirreader():
     for (root, dirs, files) in os.walk(path):
         print(root)
-
         dirs[:] = [x for x in dirs if not x.startswith('.')]
 
         for dir in dirs:
             print(os.path.join(root, dir))
-        
-        files[:] = [x for x in files if not x.startswith('.')]
+            files[:] = [x for x in files if not x.startswith('.')]
 
         for file in files:
             print(os.path.join(root, file))        
 
-# Main
+
 dirreader()
 
-# End
